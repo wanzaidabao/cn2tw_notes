363f1b41004993a5db1ba4c9eae706bac9fde783
Jim <jim@pve1.twbest.biz>
2024-07-03 03:49:51 +0800

    gitbook for book.cn2tw.online

--- docs/scripts/push-gh-pages.sh
+++ docs/scripts/push-gh-pages.sh
@@ -2,8 +2,8 @@
 # 定义项目的根目录绝对路径
 PROJECT_ROOT="/home/jim/gitrepo/cn2tw_notes"
 
-# 切换到 _book 目录
-cd "$PROJECT_ROOT/_book"
+# 切换到目录
+cd "$PROJECT_ROOT/"
 
 # 检查是否在 gh-pages 分支上
 git checkout gh-pages
@@ -18,6 +18,7 @@ git add .
 git commit -m 'gitbook for book.cn2tw.online'
 
 # 推送到远程 gh-pages 分支
-git push --set-upstream origin gh-pages
+git push --set-upstream origin gh-pages --force
 
+#git push -u origin gh-pages --force
 
