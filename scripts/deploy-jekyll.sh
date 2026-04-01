8d3600eaa1b276ad6a6e24d1eb7efbf2ad4c8dcc
TsreJJ@WSL <tsrejjj-wsl@gmail.com>
2026-04-01 17:43:03 +0800

    jim@worker03,20260401 17:42

--- scripts/deploy-jekyll.sh
+++ scripts/deploy-jekyll.sh
@@ -11,13 +11,13 @@ bundle exec jekyll build
 cd "$PROJECT_ROOT/_site"
 
 # 初始化一个新的 Git 仓库
-git init
-git checkout -b gh-pages
+#git init
+#git checkout -b gh-pages
 
 # 添加并提交更改
 git add .
 git commit -m "Deploy Jekyll site to gh-pages"
 
 # 推送到远程仓库
-git remote add origin git@github.com:wanzaidabao/gitbook.git
-git push -u origin gh-pages --force
+#git remote add origin git@github.com:wanzaidabao/gitbook.git
+#git push -u origin gh-pages --force
